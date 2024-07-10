c7c1320c48aeddee4324579cae0f1f28dd7dd3e2
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-07-10 14:51:28 +0545

    package changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { positions, transitions, Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-const options = {
-  timeout: 5000,
-  position: positions.BOTTOM_CENTER, 
-  transition: transitions.SCALE,
-};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AlertProvider template={AlertTemplate} {...options}>
+
     <App />
-  </AlertProvider>,
 )
 
