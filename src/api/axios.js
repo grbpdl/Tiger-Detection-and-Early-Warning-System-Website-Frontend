e0c659d7ceb4bff783d77c4b1635d8926663c1e5
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-07-12 12:15:57 +0545

    live deployed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://127.0.0.1:8000/';
+// const BASE_URL = 'http://127.0.0.1:8000/';
+const BASE_URL = 'http://110.44.121.73:2568/';
 
 export default axios.create({
     baseURL: BASE_URL
